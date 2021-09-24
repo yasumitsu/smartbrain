6c85e87da125f38d4c621788c99c0f01b840be4e
Kenji <kenjiyasumitsu@gmail.com>
2021-09-24 13:25:30 +0900

    add clarifai API

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import { Component } from 'react';
+
+const app = new Clarifai.App({
+	apiKey: '922c6f0ec67c49d6878b51c1bd308f19'
+});
 
 const particleOptions = {
 	number: {
@@ -15,17 +22,66 @@ const particleOptions = {
 	}
 };
 
-function App() {
-	return (
-		<div className="App">
-			<Particles params={particleOptions} className="particles" />
-			<Navigation />
-			<Logo />
-			<Rank />
-			<ImageLinkForm />
-			{/* <FaceRecognition /> */}
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			box: {}
+		};
+	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFacePosition = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputImage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFacePosition.left_col * width,
+			topRow: clarifaiFacePosition.top_row * height,
+			rightCol: width - clarifaiFacePosition.right_col * width,
+			bottomRow: height - clarifaiFacePosition.bottom_row * height
+		};
+	};
+
+	displayFaceBox = (box) => {
+		this.setState({ box });
+	};
+
+	onInputChange = (event) => {
+		this.setState({ input: event.target.value });
+	};
+
+	onButtonSubmit = () => {
+		this.setState({
+			imageUrl: this.state.input
+		});
+		app.models
+			.predict(Clarifai.CELEBRITY_MODEL, this.state.input)
+			.then((response) => {
+				this.calculateFaceLocation(response);
+				this.calculateCelebrity(response);
+				// console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+				// console.log(response.outputs[0].data.regions[0].data.concepts[0].value);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<Particles params={particleOptions} className="particles" />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+				<FaceRecognition imageUrl={this.state.imageUrl} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className="f3">{'This magic brain will detect faces in your pictures.'}</p>
 			<div className="center">
 				<div className="pa4 br3 shadow-5 center form">
-					<input className="f4 pa2 w-70 center" type="text" />
-					<button className="w-30 grow f4 link ph3 dib white bg-light-purple">Detect</button>
+					<input className="f4 pa2 w-70 center" type="text" onChange={onInputChange} />
+					<button className="w-30 grow f4 link ph3 dib white bg-light-purple" onClick={onButtonSubmit}>
+						Detect
+					</button>
 				</div>
 			</div>
 		</div>
