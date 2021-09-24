141bfa8dd27641e1868f9642c3e5b804753e642e
Kenji <kenjiyasumitsu@gmail.com>
2021-09-24 09:11:17 +0900

    add Navigation and Logo components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+function App() {
+	return (
+		<div className="App">
+			<Navigation />
+			<Logo />
+			{/* <ImageLinkForm />
+			<FaceRecognition /> */}
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Logo = () => {
+	return <div className="ma4 mt0" />;
+};
+export default Logo;
